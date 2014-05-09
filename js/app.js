282db2ba041c41b082f28ffd9ea655753da1e461
Jonesette <sjones@jjaviation.com>
2014-05-08 18:39:30 -0600

    Simplified FizzBuzz
    Removed variable j and changed push functions

--- js/app.js
+++ js/app.js
@@ -2,21 +2,17 @@ $(document).ready(function() {
 	var results = [];
 
 	for (var i=1; i<101; i++) {
-		if (i%3 === 0) {
-			var j = i;
-			if (j%5 === 0) {
-				results.push(["FizzBuzz"]);
-			}
-			else { 
-			results.push(["Fizz"]);
-			}
+		if (i%15 === 0) {
+			results.push("FizzBuzz");
 		}
-
+		else if (i%3 === 0) {
+			results.push("Fizz");
+		}
 		else if (i%5 === 0) {
-			results.push(["Buzz"]);
+			results.push("Buzz");
 		}
 		else {
-			results.push([i]);
+			results.push(i);
 		}
 
 	}
