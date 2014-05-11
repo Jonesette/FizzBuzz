00743842d6ceb60fe90fd5b9a27ee6e236624713
Jonesette <sjones@jjaviation.com>
2014-05-10 20:46:12 -0600

    Refactored to use variables and take user input
    Error on text entry not working.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,24 @@
 $(document).ready(function() {
 	var results = [];
 
-	for (var i=1; i<101; i++) {
+	var numAlert = function () {
+		alert("The Value Must be Less than 200 and Numerical!!");	
+	};
+
+	var endNumber = +prompt("Please Enter a Numerical Value Less than 200.");
+
+	while (endNumber > 200) {
+		numAlert();
+		endNumber = +prompt("Please Enter a Numerical Value Less than 200.");
+	}
+
+	while (endNumber === NaN) {
+		numAlert();
+		endNumber = +prompt("Please Enter a Numerical Value Less than 200.");
+	}
+
+
+	for (var i=1; i<endNumber; i++) {
 		if (i%15 === 0) {
 			results.push("FizzBuzz");
 		}
@@ -14,7 +31,6 @@ $(document).ready(function() {
 		else {
 			results.push(i);
 		}
-
 	}
 
 $("#solution").append('<p>'+results+'</p>');
